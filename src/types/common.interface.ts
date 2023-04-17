62f264d2d0977375d4fdf7472b5566a9101f266d
call5h5ong <call5h5ong@gmail.com>
2023-04-17 09:44:38 +0900

    입력 가능한 특수키를 정의 후 마크다운 타입에 포함
    마크다운을 입력할 때 특수키를 입력해줘야 하는 상황이 생김. 마크다운 타입에 키 타입을 포함해 특수키 입력이 가능하게 함.

--- src/types/common.interface.ts
+++ src/types/common.interface.ts
@@ -1,10 +1,20 @@
+const KeyAccelerator = {
+  ENTER: "Enter",
+  SHIFT: "Shift",
+} as const;
+type KeyAccelerator = typeof KeyAccelerator[keyof typeof KeyAccelerator];
+
+interface Key {
+  keys: KeyAccelerator[];
+}
+
 interface MarkdownContent<T> {
   index: number;
   array: T[];
 }
 
 export interface MarkdownV2 {
-  text?: MarkdownContent<string>[];
-  codeblock?: MarkdownContent<string>[];
-  list?: MarkdownContent<string>[];
+  text?: MarkdownContent<string | Key>[];
+  codeblock?: MarkdownContent<string | Key>[];
+  list?: MarkdownContent<string | Key>[];
 }
